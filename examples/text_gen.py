8b8fc279ef1bcb1e9174ff1ede501078f783a1cd
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2024-04-12 00:06:33 +0200

    fixed bugs

--- examples/text_gen.py
+++ examples/text_gen.py
@@ -4,12 +4,18 @@ from lollms_client import LollmsClient
 lc = LollmsClient("http://localhost:9600")
 
 # Generate Text
-response = lc.generate_text(prompt="Once upon a time", stream=False, temperature=0.5)
-print(response)
+# response = lc.generate_text(prompt="Once upon a time", stream=False, temperature=0.5)
+# print(response)
 
-# Generate Completion
-response = lc.generate_completion(prompt="What is the capital of France", stream=False, temperature=0.5)
-print(response)
+# # Generate Completion
+# response = lc.generate_completion(prompt="What is the capital of France", stream=False, temperature=0.5)
+# print(response)
+
+def cb(chunk, type):
+    print(chunk,end="",flush=True)
+    
+response = lc.generate_text(prompt="Once upon a time", stream=True, temperature=0.5, streaming_callback=cb)
+# print(response)
 
 # List Mounted Personalities
 response = lc.listMountedPersonalities()
--- lollms_client/lollms_core.py
+++ lollms_client/lollms_core.py
@@ -30,7 +30,7 @@ class LollmsClient():
         self.service_key = service_key
 
 
-    def generate_text(self, prompt, host_address=None, model_name=None, personality=None, n_predict=None, stream=False, temperature=0.1, top_k=50, top_p=0.95, repeat_penalty=0.8, repeat_last_n=40, seed=None, n_threads=8, service_key:str=""):
+    def generate_text(self, prompt, host_address=None, model_name=None, personality=None, n_predict=None, stream=False, temperature=0.1, top_k=50, top_p=0.95, repeat_penalty=0.8, repeat_last_n=40, seed=None, n_threads=8, service_key:str="", streaming_callback=None):
         # Set default values to instance variables if optional arguments are None
         host_address = host_address if host_address else self.host_address
         model_name = model_name if model_name else self.model_name
@@ -71,22 +71,41 @@ class LollmsClient():
             "n_threads": n_threads
         }
 
-        response = requests.post(url, json=data, headers=headers)
-
-        if response.status_code == 200:
-            try:
-                text = response.text.strip().replace('\"','"')
-                if text[0]=='"':
-                    text = text[1:]
-                if text[-1]=='"':
-                    text = text[:-1]
-                return text
-            except Exception as ex:
-                return {"status": False, "error": str(ex)}
+        response = requests.post(url, json=data, headers=headers, stream=stream)
+        if not stream:
+            if response.status_code == 200:
+                try:
+                    text = response.text.strip().replace('\"','"')
+                    if text[0]=='"':
+                        text = text[1:]
+                    if text[-1]=='"':
+                        text = text[:-1]
+                    return text
+                except Exception as ex:
+                    return {"status": False, "error": str(ex)}
+            else:
+                return {"status": False, "error": response.text}
         else:
-            return {"status": False, "error": response.text}
+            text = ""
+            if response.status_code==200:
+                try:
+                    for line in response.iter_lines():
+                        chunk = line.decode("utf-8")
+                        text += chunk
+                        if streaming_callback:
+                            streaming_callback(chunk, MSG_TYPE.MSG_TYPE_CHUNK)
+                    if text[0]=='"':
+                        text = text[1:]
+                    if text[-1]=='"':
+                        text = text[:-1]
+                    return text
+                except Exception as ex:
+                    return {"status": False, "error": str(ex)}
+            else:
+                return {"status": False, "error": response.text}
 
-    def generate_completion(self, prompt, host_address=None, model_name=None, personality=None, n_predict=None, stream=False, temperature=0.1, top_k=50, top_p=0.95, repeat_penalty=0.8, repeat_last_n=40, seed=None, n_threads=8, completion_format="vllm", service_key:str="", streaming_callback=None):
+
+    def generate_completion(self, prompt, host_address=None, model_name=None, personality=None, n_predict=None, stream=False, temperature=0.1, top_k=50, top_p=0.95, repeat_penalty=0.8, repeat_last_n=40, seed=None, n_threads=8, completion_format="vllm instruct", service_key:str="", streaming_callback=None):
         # Set default values to instance variables if optional arguments are None
         host_address = host_address if host_address else self.host_address
         model_name = model_name if model_name else self.model_name
