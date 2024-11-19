1d27c54ac5559b9291b1c8d551f385929808ee7e
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2024-11-19 18:17:38 +0100

    update

--- examples/test_local_models/local_chat.py
+++ examples/test_local_models/local_chat.py
@@ -1,7 +1,6 @@
-from lollms_client import LollmsClient, ELF_GENERATION_FORMAT, TasksLibrary, FunctionCalling_Library
-import random
+from lollms_client import LollmsClient, ELF_GENERATION_FORMAT
 
-# Initialize the LollmsClient instance
+# Initialize the LollmsClient instance please select a different model to test with
 lc = LollmsClient(model_name= r"E:\drumber\LOLLMS_AWARE_LLAMA_mi_lord",default_generation_mode=ELF_GENERATION_FORMAT.TRANSFORMERS)
 def cb(text, msg_type=0):
     print(text,end='', flush=True)
