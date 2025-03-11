e0f77edf6bb4dce3020f923fd57972e2ca7510e5
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2025-03-11 21:22:07 +0100

    upgraded

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt', 'r') as f:
     
 setuptools.setup(
     name="lollms_client",
-    version="0.9.1",
+    version="0.9.2",
     author="ParisNeo",
     author_email="parisneoai@gmail.com",
     description="A client library for LoLLMs generate endpoint",
