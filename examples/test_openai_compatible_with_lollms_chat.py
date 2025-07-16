e9f47d6afa9bd77feb2d02e6f9095de950363005
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2025-07-16 13:02:39 +0200

    Create test_openai_compatible_with_lollms_chat.py

---
+++ examples/test_openai_compatible_with_lollms_chat.py
@@ -0,0 +1,12 @@
+from lollms_client import LollmsClient
+#make sure you use your key
+lc = LollmsClient("openai","http://localhost:9642/v1/", service_key="lollms_zXQdyvrP_ecMXm3UZ0D004x979aHpyF8iq4ki_b52q0WdFuiEfMo")
+models = lc.listModels()
+print(f"Found models:\n{models}")
+
+lc.set_model_name("litellm/mistralsmall-22b")
+
+res = lc.generate_text("Describe this image",images=[
+    r"C:\Users\sa226037\Pictures\Charles_de_Gaulle-1963.jpg"
+])
+print(res)
