d08e00e74b8ae501f266874a500427b96af82671
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2025-07-16 17:19:22 +0200

    Update test_openai_compatible_with_lollms_chat.py

--- examples/test_openai_compatible_with_lollms_chat.py
+++ examples/test_openai_compatible_with_lollms_chat.py
@@ -4,10 +4,9 @@ lc = LollmsClient("openai","http://localhost:9642/v1/", service_key="lollms_zXQd
 models = lc.listModels()
 print(f"Found models:\n{models}")
 
-# format is: binding name/model name.
-lc.set_model_name("litellm/mistralsmall-22b")
+lc.set_model_name("ollama/gemma3:27b")
 
 res = lc.generate_text("Describe this image",images=[
-    r"C:\Users\Pictures\Charles_de_Gaulle-1963.jpg"
+    r"C:\Users\parisneo\Pictures\me.jpg"
 ])
 print(res)
