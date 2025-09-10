c5212accd380481d6b80a8fc6778652c8bee02dd
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2025-09-11 01:49:32 +0200

    test

--- examples/text_gen_ollama.py
+++ examples/text_gen_ollama.py
@@ -4,16 +4,16 @@ from lollms_client import LollmsClient, MSG_TYPE
 # Initialize the LollmsClient instance
 # two possible ways: 
 # Secure using my ollama proxy Server Fortress (https://github.com/ParisNeo/ollama_proxy_server)
-lc = LollmsClient(llm_binding_name="ollama", llm_binding_config={
-        "host_address": "http://localhost:8080",
-        "model_name": "gpt-oss:20b",
-        "service_key": "op_85f467a44ba90bec_02a106ef01d83e9d14eeb7dfcd32634b1bef9d160e3f47d9",# change this to your actual service key
-    })
-#Not secure
 # lc = LollmsClient(llm_binding_name="ollama", llm_binding_config={
-#         "host_address": "http://localhost:11434",
+#         "host_address": "http://localhost:8080",
 #         "model_name": "gpt-oss:20b",
+#         "service_key": "op_85f467a44ba90bec_02a106ef01d83e9d14eeb7dfcd32634b1bef9d160e3f47d9",# change this to your actual service key
 #     })
+#Not secure
+lc = LollmsClient(llm_binding_name="ollama", llm_binding_config={
+        "host_address": "http://localhost:11434",
+        "model_name": "gpt-oss:20b",
+    })
 
 def cb(data:str|dict, type:MSG_TYPE)->bool:
     if type == MSG_TYPE.MSG_TYPE_CHUNK:
